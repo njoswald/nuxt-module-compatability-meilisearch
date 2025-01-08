235970fc200960f0fd19a6bc65470fb8e4c0fff7
Nick Oswald <oswaldn2000i@gmail.com>
2025-01-08 16:37:02 -0600

    Removed un-needed comments.

--- app.vue
+++ app.vue
@@ -1,14 +1,8 @@
 <template>
   <div>
     <NuxtWelcome />
-    <!-- {{ result }} -->
   </div>
 </template>
 
 <script setup>
-// const { search, result } = useMeiliSearch(useRuntimeConfig().public.MEILISEARCH_BOND_PRODUCT_INDEX || "Bond-Products");
-
-// onMounted(async () => {
-//   await search('Test');
-// })
 </script>
